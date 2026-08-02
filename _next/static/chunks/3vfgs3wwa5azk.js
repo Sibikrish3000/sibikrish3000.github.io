003da3ae0c71c441d5526f5ba9584901067858b7
Sibikrish3000 <Sibikrish3000@users.noreply.github.com>
2026-08-02 13:52:05 +0000

    deploy: Sibikrish3000/portfolio-new@46fcf26c321bf741155cee01faf5819e88964c1a

---
+++ _next/static/chunks/3vfgs3wwa5azk.js
@@ -0,0 +1,1 @@
+(globalThis.TURBOPACK||(globalThis.TURBOPACK=[])).push(["object"==typeof document?document.currentScript:void 0,22179,s=>{s.v(t=>Promise.all(["static/chunks/134h4sbcl6cj1.js"].map(t=>s.l(t))).then(()=>t(13249)))},32700,s=>{s.v(t=>Promise.all(["static/chunks/0vbme_qw8hddp.js"].map(t=>s.l(t))).then(()=>t(94388)))}]);
